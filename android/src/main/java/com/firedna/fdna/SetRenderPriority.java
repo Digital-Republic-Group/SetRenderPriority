d770fde32ab1b8b60ff7d538e6e082b933374dc5
Ian Bennett <ian@digital-republic.co.uk>
2024-05-10 14:01:31 +0100

    Started

--- android/src/main/java/com/firedna/fdna/SetRenderPriority.java
+++ android/src/main/java/com/firedna/fdna/SetRenderPriority.java
@@ -6,6 +6,7 @@ public class SetRenderPriority {
 
     public String echo(String value) {
         Log.i("Echo", value);
+        webView.setRendererPriorityPolicy(WebView.RENDERER_PRIORITY_IMPORTANT, false);
         return value;
     }
 }
